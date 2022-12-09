288594dfd5f7551a36e262a10a60652e26e2490e
emTr0 <em@emtr0.com>
2022-12-09 21:53:30 +0000

    Updates to end of Chapter 10.

--- src/products/models.py
+++ src/products/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Product(models.Model):
     '''Products Class'''
-    title = models.TextField()
-    description = models.TextField()
-    price = models.TextField()
-    summary = models.TextField(default='This is cool!')
+    title = models.CharField(max_length=120)  # max_length = required
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10000)
+    summary = models.TextField()
