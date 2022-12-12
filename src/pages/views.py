6e9d643d60d49e5da4cdf3a0988d18a46866b0f4
emTr0 <em@emtr0.com>
2022-12-12 19:01:56 +0000

    Updates through end of Chapter 19.

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
 def home_view(request, *args, **kwargs):  # *args, **kwargs
     # return HttpResponse("<h1>Hello World</h1>")  # string of HTML code
     return render(request, "home.html", {})
@@ -14,4 +13,10 @@ def contact_view(request, *args, **kwargs):
 
 
 def about_view(request, *args, **kwargs):
-    return render(request, "about.html", {})
+    my_context = {
+        "my_text": "This is about us",
+        "this_is_true": True,
+        "my_number": 123,
+        "my_list": [123, 456, 312, "Abc"]
+    }
+    return render(request, "about.html", my_context)
