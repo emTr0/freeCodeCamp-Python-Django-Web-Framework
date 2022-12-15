d91d78df508b19056cf41435c8d092547d8e1441
emTr0 <em@emtr0.com>
2022-12-15 18:55:03 +0000

    Changes up to end of Chapter 25

--- src/products/views.py
+++ src/products/views.py
@@ -7,10 +7,17 @@ from .models import Product
 
 def product_create_view(request):
     my_form = RawProductForm()
+    if request.method == 'POST':
+        my_form = RawProductForm(request.POST)
+        if my_form.is_valid():
+            print(my_form.cleaned_data)
+            Product.objects.create(**my_form.cleaned_data)
+        else:
+            print(my_form.errors)
     context = {
         'form': my_form
     }
-    return render(request, "products/product_create.html", context)
+    return render(request, 'products/product_create.html', context)
 
 # def product_create_view(request):
 #     # print(request.GET)
