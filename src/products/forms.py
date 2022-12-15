d4f358043cac79e7e3673302e319cd3ae7580943
emTr0 <em@emtr0.com>
2022-12-15 23:56:11 +0000

    Changes up to end of Chapter 26

--- src/products/forms.py
+++ src/products/forms.py
@@ -10,8 +10,19 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
-        
+
 class RawProductForm(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField()
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Your title'}))
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                'placeholder': 'Your description',
+                'class': 'new-class-name two',
+                'id': 'my-id-for-textarea',
+                'rows': 20,
+                'cols': 120
+            }
+        )
+    )
     price = forms.DecimalField()
