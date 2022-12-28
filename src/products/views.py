a72d4e9b1ce03b74ce61961755745b2c01af6953
emTr0 <em@emtr0.com>
2022-12-28 23:44:07 +0000

    Updates to end of Chapter 27

--- src/products/views.py
+++ src/products/views.py
@@ -5,19 +5,19 @@ from .forms import ProductForm, RawProductForm
 from .models import Product
 # Create your views here.
 
-def product_create_view(request):
-    my_form = RawProductForm()
-    if request.method == 'POST':
-        my_form = RawProductForm(request.POST)
-        if my_form.is_valid():
-            print(my_form.cleaned_data)
-            Product.objects.create(**my_form.cleaned_data)
-        else:
-            print(my_form.errors)
-    context = {
-        'form': my_form
-    }
-    return render(request, 'products/product_create.html', context)
+# def product_create_view(request):
+#     my_form = RawProductForm()
+#     if request.method == 'POST':
+#         my_form = RawProductForm(request.POST)
+#         if my_form.is_valid():
+#             print(my_form.cleaned_data)
+#             Product.objects.create(**my_form.cleaned_data)
+#         else:
+#             print(my_form.errors)
+#     context = {
+#         'form': my_form
+#     }
+#     return render(request, 'products/product_create.html', context)
 
 # def product_create_view(request):
 #     # print(request.GET)
@@ -28,15 +28,15 @@ def product_create_view(request):
 #     context = {}
 #     return render(request, "products/product_create.html", context)
 
-# def product_create_view(request):
-#     form = ProductForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         form = ProductForm()
-#     context = {
-#         'form': form
-#     }
-#     return render(request, "products/product_create.html", context)
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
