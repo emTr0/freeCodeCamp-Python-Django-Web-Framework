3110e6a5b6c4d99da432695aae3a4219f048e4ab
emTr0 <em@emtr0.com>
2023-02-06 21:38:46 +0000

    Changes up to 03:36:03

--- src/courses/views.py
+++ src/courses/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 
+from .forms import CourseModelForm
 from .models import Course
 # BASE VIEW CLASS = VIEW
+
+class CourseCreateView(View):
+    template_name = 'courses/course_create.html' # DetailView
+    def get(self, request, *args, **kwargs):
+        # GET method
+        form = CourseModelForm()
+        context = {'form': form}
+        return render(request, self.template_name, context)
+    
+    def post(self, request, *args, **kwargs):
+        # POST method
+        form = CourseModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+        context = {'form': form}
+        return render(request, self.template_name, context)
+
+class CourseListView(View):
+    template_name = 'courses/course_list.html'
+    queryset = Course.objects.all()
+    
+    def get_queryset(self):
+        return self.queryset
+
+    def get(self, request, *args, **kwargs):
+        context = {'object_list': self.get_queryset()}
+        return render(request, self.template_name, context)
+    
 
 class CourseView(View):
     template_name = 'courses/course_detail.html' # DetailView
@@ -12,7 +41,7 @@ class CourseView(View):
         if id is not None:
             obj = get_object_or_404(Course, id=id)
             context['object'] = obj
-        return render(request, self.template_name, {})
+        return render(request, self.template_name, context)
 
     # def post(request, *args, **kwargs):
     #     return render(request, 'about.html', {})
@@ -21,4 +50,5 @@ class CourseView(View):
 # HTTP METHODS
 
 def my_fbv(request, *args, **kwargs):
+    print(request.method)
     return render(request, 'about.html', {})
