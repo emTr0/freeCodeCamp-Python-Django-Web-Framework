34c858c5ba3dc0be7806199f633a36484cc70a01
emTr0 <em@emtr0.com>
2023-02-09 21:24:56 +0000

    Final update at end of course.

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -3,6 +3,8 @@ from .views import (
     CourseView,
     CourseListView,
     CourseCreateView,
+    CourseUpdateView,
+    CourseDeleteView,
     # my_fbv
 )
 
@@ -14,6 +16,6 @@ urlpatterns = [
 
     path('create/', CourseCreateView.as_view(), name='courses-create'),
     path('<int:id>/', CourseView.as_view(), name='courses-detail'),
-    # path('<int:id>/update/', CourseUpdateView.as_view(), name='courses-update'),
-    # path('<int:id>/delete/', CourseDeleteView.as_view(), name='courses-delete'),
+    path('<int:id>/update/', CourseUpdateView.as_view(), name='courses-update'),
+    path('<int:id>/delete/', CourseDeleteView.as_view(), name='courses-delete'),
 ]
--- src/courses/views.py
+++ src/courses/views.py
@@ -1,16 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 
 from .forms import CourseModelForm
 from .models import Course
 # BASE VIEW CLASS = VIEW
 
+
+class CourseObjectMixin(object):
+    model = Course
+    def get_object(self):
+        id = self.kwargs.get('id')
+        obj = None
+        if id is not None:
+            obj = get_object_or_404(self.model, id=id)
+        return obj
+
+
 class CourseCreateView(View):
-    template_name = 'courses/course_create.html' # DetailView
+    template_name = "courses/course_create.html" # DetailView
     def get(self, request, *args, **kwargs):
         # GET method
         form = CourseModelForm()
-        context = {'form': form}
+        context = {"form": form}
         return render(request, self.template_name, context)
     
     def post(self, request, *args, **kwargs):
@@ -18,6 +29,7 @@ class CourseCreateView(View):
         form = CourseModelForm(request.POST)
         if form.is_valid():
             form.save()
+            form = CourseModelForm()
         context = {'form': form}
         return render(request, self.template_name, context)
 
@@ -33,19 +45,61 @@ class CourseListView(View):
         return render(request, self.template_name, context)
     
 
-class CourseView(View):
-    template_name = 'courses/course_detail.html' # DetailView
+class CourseView(CourseObjectMixin, View):
+    template_name = "courses/course_detail.html" # DetailView
     def get(self, request, id=None, *args, **kwargs):
         # GET method
-        context = {}
-        if id is not None:
-            obj = get_object_or_404(Course, id=id)
-            context['object'] = obj
+        context = {'object': self.get_object()}
         return render(request, self.template_name, context)
 
     # def post(request, *args, **kwargs):
     #     return render(request, 'about.html', {})
 
+
+class CourseUpdateView(CourseObjectMixin, View):
+    template_name = "courses/course_update.html" # DetailView
+    def get(self, request, id=None, *args, **kwargs):
+        # GET method
+        context = {}
+        obj = self.get_object()
+        if obj is not None:
+            form = CourseModelForm(instance=obj)
+            context['object'] = obj
+            context['form'] = form
+        return render(request, self.template_name, context)
+
+    def post(self, request, id=None,  *args, **kwargs):
+        # POST method
+        context = {}
+        obj = self.get_object()
+        if obj is not None:
+            form = CourseModelForm(request.POST, instance=obj)
+            if form.is_valid():
+                form.save()
+            context['object'] = obj
+            context['form'] = form
+        return render(request, self.template_name, context)
+
+
+class CourseDeleteView(CourseObjectMixin, View):
+    template_name = "courses/course_delete.html" # DetailView
+    def get(self, request, id=None, *args, **kwargs):
+        # GET method
+        context = {}
+        obj = self.get_object()
+        if obj is not None:
+            context['object'] = obj
+        return render(request, self.template_name, context)
+
+    def post(self, request, id=None,  *args, **kwargs):
+        # POST method
+        context = {}
+        obj = self.get_object()
+        if obj is not None:
+            obj.delete()
+            context['object'] = None
+            return redirect('/courses/')
+        return render(request, self.template_name, context)
 
 # HTTP METHODS
 
